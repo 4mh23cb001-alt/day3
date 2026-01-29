474a02c1c3fedeb242a4e7981f9db067339a12e5
4mh23cb001-alt <4mh23cb001@gmail.com>
2026-01-29 15:42:41 +0530

    Add files via upload

---
+++ code6.java
@@ -0,0 +1,15 @@
+public class code6 {
+     public static void main(String[]args) {
+        int i=2;
+
+        if (i%2==0){
+        System.out.println("it is even");
+        }
+        else{
+            System.out.println("it's odd");
+        }
+
+        }
+
+
+    }
---
+++ code7.java
@@ -0,0 +1,11 @@
+public class code7 {
+     public static void main(String[]args) {
+        int i=2;
+        int j=5;
+        if (i>j){
+        System.out.println("I is greter");
+        }else{
+            System.out.println("J is greter");
+    }
+}
+}
